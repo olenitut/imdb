3e8f4fc53a23696bc1d5bbd804b36b133ac43452
Ana Hordieieva <gordeevaisgreat@gmail.com>
2023-09-27 18:48:52 +0300

    add loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="flex items-center justify-center h-96">
+      <div className="lds-dual-ring"></div>
+    </div>
+  );
+};
+
+export default loading;
